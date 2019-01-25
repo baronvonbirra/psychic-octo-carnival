440d9384dd69dc11a3530eb715032a9ded8fe1b6
BaronVonBirra <soul-to-squeeze@hotmail.com>
2019-01-25 15:38:54 +0100

    Use new API

--- js/code.js
+++ js/code.js
@@ -1,22 +1,44 @@
 var getActorCall = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api.themoviedb.org/3/find/nm0000409?external_source=imdb_id&language=en-US&api_key=44f63178760c028c7269aab4ef019b6f",
-    "method": "GET",
-    "headers": {},
-    "data": "{}"
+  "async": true,
+  "crossDomain": true,
+  "url": generateURL(),
+  "method": "GET",
+  "headers": {},
+  "data": "{}"
   }
   
-var getActorName = $.ajax(getActorCall).done(function (response) {
-    getName = response.person_results[0].name
+var getActor1Name = $.ajax(getActorCall).done(function (response) {
+  getName = response.name
 
-    document.getElementById("actorOne").innerHTML = getName;
+  getActorOneName();
 });
 
+var getActo2rName = $.ajax(getActorCall).done(function (response) {
+  getName = response.name
+
+  getActorTwoName();
+});
+
+function getActorOneName() {
+  document.getElementById("actorOne").innerHTML = getName;
+}
+
+function getActorTwoName() {
+  document.getElementById("actorTwo").innerHTML = getName;
+}
+
 function generateRandomNumber() {
-    var number =  Math.random() * (9999999);
-    number = Math.round(number);
-    console.log(number);
+  var number =  Math.random() * (9999);
+  number = Math.round(number);
+  number = number.toString();
+  console.log(number);
     
-    return number;
+  return number;
+}
+
+function generateURL() {
+  var number = generateRandomNumber();
+  var url = "https://api.themoviedb.org/3/person/"+number+"?language=en-US&api_key=44f63178760c028c7269aab4ef019b6f";
+
+  return url;
 }
