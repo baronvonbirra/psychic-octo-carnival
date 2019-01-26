97cbec528dd34fef2048b67fc1121fbd58bca0d2
BaronVonBirra <soul-to-squeeze@hotmail.com>
2019-01-26 13:24:35 +0100

    Generating 2 names

--- js/code.js
+++ js/code.js
@@ -1,43 +1,62 @@
-var getActorCall = {
+var getActorOneCall = {
   "async": true,
   "crossDomain": true,
-  "url": generateURL(),
+  "url": getActorURL(),
   "method": "GET",
   "headers": {},
   "data": "{}"
-  }
+  };
+
+var getActorOneName = $.ajax(getActorOneCall).done(function (response) {
+  getNameOne = response.name
+  printActorOneName()
+});
+
+var getActorTwoCall = {
+  "async": true,
+  "crossDomain": true,
+  "url": getActorURL(),
+  "method": "GET",
+  "headers": {},
+  "data": "{}"
+  };  
+
+var getActorTwoName = $.ajax(getActorTwoCall).done(function (response) {
+  getNameTwo = response.name
+  printActorTwoName()
+});  
   
-var getActor1Name = $.ajax(getActorCall).done(function (response) {
-  getName = response.name
-
-  getActorOneName();
-});
-
-var getActo2rName = $.ajax(getActorCall).done(function (response) {
-  getName = response.name
-
-  getActorTwoName();
-});
-
-function getActorOneName() {
-  document.getElementById("actorOne").innerHTML = getName;
+function printActorOneName() {
+  document.getElementById("actorOne").innerHTML = getNameOne;
 }
+
+function printActorTwoName() {
+  document.getElementById("actorTwo").innerHTML = getNameTwo;
+}
+
+function getActorURL() {
+  number = generateRandomNumber();
+  url = generateURL(number);
+
+  return url;
+};
 
 function getActorTwoName() {
-  document.getElementById("actorTwo").innerHTML = getName;
-}
+  number = generateRandomNumber();
+  url = generateURL(number);
+
+  return url;
+};
 
 function generateRandomNumber() {
   var number =  Math.random() * (9999);
   number = Math.round(number);
   number = number.toString();
-  console.log(number);
     
   return number;
 }
 
-function generateURL() {
-  var number = generateRandomNumber();
+function generateURL(number) {
   var url = "https://api.themoviedb.org/3/person/"+number+"?language=en-US&api_key=44f63178760c028c7269aab4ef019b6f";
 
   return url;
